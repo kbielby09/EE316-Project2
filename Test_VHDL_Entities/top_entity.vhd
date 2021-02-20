2913a68a2b3bcc467837866055f7240b58d8fcb8
kbielby09 <kbielby09@gmail.com>
2021-02-19 19:35:03 -0500

    added i2c componenet

--- Test_VHDL_Entities/top_entity.vhd
+++ Test_VHDL_Entities/top_entity.vhd
@@ -56,6 +56,25 @@ architecture rtl of top_entity is
   	);
   end component ROM;
 
+  -- TODO implement
+  -- component i2c_master IS
+  --   generic(
+  --     input_clk : integer := 100_000_000; --input clock speed from user logic in Hz
+  --     bus_clk   : integer := 400_000);   --speed the i2c bus (scl) will run at in Hz
+  --   port(
+  --     clk       : in     std_logic;                    --system clock
+  --     reset_n   : in     std_logic;                    --active low reset
+  --     ena       : in     std_logic;                    --latch in command
+  --     addr      : in     std_logic_vector(6 DOWNTO 0); --address of target slave
+  --     rw        : in     std_logic;                    --'0' is write, '1' is read
+  --     data_wr   : in     std_logic_vector(7 DOWNTO 0); --data to write to slave
+  --     busy      : out    std_logic;                    --indicates transaction in progress
+  --     data_rd   : out    std_logic_vector(7 DOWNTO 0); --data read from slave
+  --     ack_error : buffer std_logic;                    --flag if improper acknowledge from slave
+  --     sda       : inout  std_logic;                    --serial data output of i2c bus
+  --     scl       : inout  std_logic);                   --serial clock output of i2c bus
+  -- end component i2c_master;
+
   -- ROM initialization signal
   signal rom_initialize  : std_logic := '0';
   signal rom_data        : std_logic_vector(15 downto 0);
