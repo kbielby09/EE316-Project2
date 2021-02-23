b42fc6bb617079f4ecb7aad5c8850a6fd8296bab
kbielby09 <63510934+kbielby09@users.noreply.github.com>
2021-02-23 10:08:43 -0500

    fixed errors with i2c user logic

--- Final_Product/user_logic.vhd
+++ Final_Product/user_logic.vhd
@@ -14,13 +14,15 @@ end usr_logic;
 architecture behavior of usr_logic is
 
 signal Cont 		: unsigned(19 downto 0) := X"03FFF";
+
 type state_type is (start, write_data, repeat);
-signal slave_addr : std_logic_vector(6 downto 0) := "1110001";
-signal i2c_addr : std_logic_vector(6 downto 0);
 signal state : state_type := start;
+
+-- signal slave_addr : std_logic_vector(6 downto 0) := "1110001"; -- Not Needed
+signal i2c_addr : std_logic_vector(6 downto 0);
 signal regBusy, sigBusy, reset_n, i2c_ena, i2c_rw, ack_err : std_logic;
 signal data_wr: std_logic_vector(7 downto 0);
-signal i2c_data_wr : std_logic_vector(7 downto 0);
+-- signal i2c_data_wr : std_logic_vector(7 downto 0); -- THIS IS NOT EVEN BEING USED
 signal byteSel : integer := 0;
 signal regData: std_logic_vector(15 downto 0);
 signal busy_prev : std_logic;
@@ -60,14 +62,16 @@ port map(
 	rw => i2c_rw,
 	data_wr => data_wr,
 	busy => sigBusy,
-	data_rd => OPEN,
+	data_rd => OPEN,      -- WHATS THIS?
 	ack_error => ack_err,
 	sda => oSDA,
 	scl => oSCL);
 
 process(clk)
 begin
-if(clk'EVENT and clk = '1') then
+if(rising_edge(clk)) then
+	regData <= iData;
+	regBusy <= sigBusy;
 	case state is
 		when start =>
 			if Cont /= X"00000" then
@@ -78,34 +82,31 @@ if(clk'EVENT and clk = '1') then
 			else
 				reset_n <= '1';
 				i2c_ena <= '1';
-				i2c_addr <= slave_addr;
+				i2c_addr <= "1110001";
 				i2c_rw <= '0';
-				i2c_data_wr <= data_wr;
+				-- data_wr <= i2c_data_wr; -- This line really doesn't do anything
 				state <= write_data;
 			end if;
 		when write_data =>
-		regBusy <= sigBusy;
-		regData<=iData;
+		-- regBusy <= sigBusy;
+		-- regData <= iData;
 		if regBusy/=sigBusy and sigBusy='0' then
 			if byteSel /= 12 then
 				byteSel <= byteSel + 1;
 				state <= write_data;
-
 			else
-				byteSel<= 8;
+				byteSel <= 9;
 				i2c_ena <= '0';
 				state <= repeat;
 			end if;
 		end if;
-
-		when repeat =>
+		when repeat => -- wait for new data
 			i2c_ena <= '0';
 			if regData /= iData then
 				Cont <= X"03FFF";
 				state <= start;
 			else
 				state <= repeat;
-
 			end if;
 		end case;
 end if;
@@ -117,19 +118,18 @@ begin
 		when 0 => data_wr <= X"76";
 		when 1 => data_wr <= X"76";
 		when 2 => data_wr <= X"76";
-		when 3 => data_wr <= X"7A";
+		when 3 => data_wr <= X"7A"; -- move this up
 		when 4 => data_wr <= X"FF";
-		when 5 => data_wr <= X"77";
-		when 6 => data_wr <= X"00";
+		when 5 => data_wr <= X"77";  -- ox79
+		when 6 => data_wr <= X"00";  -- 0
 		when 7 => data_wr <= X"79";
 		when 8 => data_wr <= X"00";
-		when 9 => data_wr <= X"0"&iData(15 downto 12);
-		when 10 => data_wr <= X"0"&iData(11 downto 8);
-		when 11 => data_wr <= X"0"&iData(7 downto 4);
-		when 12 => data_wr <= X"0"&iData(3 downto 0);
+		when 9 => data_wr <= "0000"&iData(15 downto 12);
+		when 10 => data_wr <= "0000"&iData(11 downto 8);
+		when 11 => data_wr <= "0000"&iData(7 downto 4);
+		when 12 => data_wr <= "0000"&iData(3 downto 0);
 		when others => data_wr <= X"76";
 	end case;
 end process;
-
 
 end behavior;
